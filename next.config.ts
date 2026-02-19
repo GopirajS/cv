5eb3dd4bc05fddef90bedd06361be0b66e50ea05
Gopi Raj <gopiraj@elitoinnovations.com>
2026-02-19 11:39:11 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* basePath: '/your-repo-name', // Uncomment and set your repository name for GitHub Pages subdirectory deployment */
   output: "export",
   images: {
     unoptimized: true,
